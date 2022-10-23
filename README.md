bbac32ff7839dc1d06c9d42f8176e2575a7d474c
wanyyyy <wan1471@gmail.com>
2022-10-23 15:27:25 +0900

    commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-리액트 공부용
+리액트 공부
+
+라이브러리
+1. npm i react-router-dom
+2. npm i react-responsive-carousel
+3. <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous"/>
+4. npm i react-loading-skeleton
+
+
+
+참고 유튜브 : https://youtu.be/KH-pw1cv34E
--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Header from './components/header/Header';
+import Home from './pages/home/home';
+import MovieList from './components/movieList/movieList';
 
 function App() {
   return (
@@ -8,9 +10,9 @@ function App() {
         <Router>
           <Header />
             <Routes>
-                <Route index element={<h1>asdf</h1>}></Route>
+                <Route index element={<Home />}></Route>
                 <Route path="movie/:id" element={<h1>123</h1>}></Route>
-                <Route path="movies/:type" element={<h1>123</h1>}></Route>
+                <Route path="movies/:type" element={<MovieList/>}></Route>
                 <Route path="/*" element={<h1>Error Page</h1>}></Route>
             </Routes>
         </Router>
